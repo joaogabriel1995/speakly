7d2463ce6b1dec8fb0fd94451a289e08591829f8
joaogabriel1995 <joao.pereira@gtiit.com.br>
2025-02-22 21:07:59 -0300

    Remove unused imports from server index file

--- backend/src/infrastructure/server/index.ts
+++ backend/src/infrastructure/server/index.ts
@@ -1,7 +1,4 @@
 import express from 'express'
-import { EvolutionController } from '../../application/controllers/evolutionController'
-import { HandleChatInputUseCase } from '../../application/useCases/handleChatInputUseCase'
-import { WhatsAppAdapter } from '../adapters/evolutionAppAdapter'
 import { router } from './routes'
 
 
