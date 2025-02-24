6d83125d7841e186e8aa6c2d094d8550e38f6497
joaogabriel1995 <joao.pereira@gtiit.com.br>
2025-02-24 18:07:32 -0300

    Add EnvLoader class for environment variable validation using Zod schema

---
+++ backend/src/infrastructure/config/envLoader.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export interface IEnvLoader<T> {
+  load(): T
+}
+
+export class EnvLoader<T extends z.ZodObject<any>> implements IEnvLoader<z.infer<T>> {
+  constructor(private schema: T) { }
+  load(): z.infer<T> {
+    try {
+      return this.schema.parse(process.env);
+    } catch (error) {
+      throw new Error(`Erro ao validar configuração: ${(error as Error).message}`);
+    }
+  }
+}
+
+
+
