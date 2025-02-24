db7bcaaa7dd049b9b5832fcf7b7f42f8159cd4df
joaogabriel1995 <joao.pereira@gtiit.com.br>
2025-02-24 18:06:39 -0300

    Add AppConversationOrchestratorUseCase to handle chat and voice input processing

---
+++ backend/src/application/useCases/appConversationOrchestratorUseCase.ts
@@ -0,0 +1,22 @@
+import { IWebhookAdapter } from "../interfaces/IWebhookAdapter";
+import { messageTypeEnum, WebhookBodySchemaType } from "../schemas/evoluctionWebhookSchema";
+import { HandleChatInputUseCase } from "./handleChatInputUseCase";
+import { HandleVoiceInputUseCase } from "./handleVoiceInputUseCase";
+
+export class AppConversationOrchestratorUseCase {
+    constructor(
+        private readonly handleChatInputUseCase: HandleChatInputUseCase,
+        private readonly handleVoiceInputUseCase: HandleVoiceInputUseCase,
+        private readonly webhookAdapter: IWebhookAdapter<WebhookBodySchemaType>
+    ) { }
+    async execute(webhookPayloadBody: WebhookBodySchemaType) {
+        const { messageType } = webhookPayloadBody.data
+        if (messageType === messageTypeEnum.enum.conversation) {
+            const message = this.webhookAdapter.toMessage(webhookPayloadBody)
+            await this.handleChatInputUseCase.execute(message);
+        }
+        if (messageType === messageTypeEnum.Enum.audioMessage) {
+            this.handleVoiceInputUseCase.execute(webhookPayloadBody)
+        }
+    }
+}
