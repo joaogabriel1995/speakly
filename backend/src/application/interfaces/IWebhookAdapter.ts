20139aa0979fdd52ff6d50e8b1345f8b81d4bc54
joaogabriel1995 <joao.pereira@gtiit.com.br>
2025-02-24 18:06:27 -0300

    Remove unused imports from IWebhookAdapter interface

--- backend/src/application/interfaces/IWebhookAdapter.ts
+++ backend/src/application/interfaces/IWebhookAdapter.ts
@@ -1,5 +1,4 @@
 import { Message } from "../../domain/entities/message";
-import { Request, Response } from "./Controller";
 
 export interface IWebhookAdapter<T> {
     toMessage(data: T): Message;
