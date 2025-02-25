6835abd67ca2286b9cdbf15c5c032e225a51ce6c
joaogabriel1995 <joao.pereira@gtiit.com.br>
2025-02-25 16:24:15 -0300

    Add logging statement in AppConversationOrchestratorUseCase for debugging

--- backend/src/application/useCases/appConversationOrchestratorUseCase.ts
+++ backend/src/application/useCases/appConversationOrchestratorUseCase.ts
@@ -4,19 +4,20 @@ import { HandleChatInputUseCase } from "./handleChatInputUseCase";
 import { HandleVoiceInputUseCase } from "./handleVoiceInputUseCase";
 
 export class AppConversationOrchestratorUseCase {
-    constructor(
-        private readonly handleChatInputUseCase: HandleChatInputUseCase,
-        private readonly handleVoiceInputUseCase: HandleVoiceInputUseCase,
-        private readonly webhookAdapter: IWebhookAdapter<WebhookBodySchemaType>
-    ) { }
-    async execute(webhookPayloadBody: WebhookBodySchemaType) {
-        const { messageType } = webhookPayloadBody.data
-        if (messageType === messageTypeEnum.enum.conversation) {
-            const message = this.webhookAdapter.toMessage(webhookPayloadBody)
-            await this.handleChatInputUseCase.execute(message);
-        }
-        if (messageType === messageTypeEnum.Enum.audioMessage) {
-            this.handleVoiceInputUseCase.execute(webhookPayloadBody)
-        }
+  constructor(
+    private readonly handleChatInputUseCase: HandleChatInputUseCase,
+    private readonly handleVoiceInputUseCase: HandleVoiceInputUseCase,
+    private readonly webhookAdapter: IWebhookAdapter<WebhookBodySchemaType>
+  ) { }
+  async execute(webhookPayloadBody: WebhookBodySchemaType) {
+    console.log("ASOIDJAIOSDJ")
+    const { messageType } = webhookPayloadBody.data
+    if (messageType === messageTypeEnum.enum.conversation) {
+      const message = this.webhookAdapter.toMessage(webhookPayloadBody)
+      await this.handleChatInputUseCase.execute(message);
     }
+    if (messageType === messageTypeEnum.enum.audioMessage) {
+      this.handleVoiceInputUseCase.execute(webhookPayloadBody)
+    }
+  }
 }
