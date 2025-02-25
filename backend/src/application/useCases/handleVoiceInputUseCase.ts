072c26dee908c77bbee00cfb1a63b39491924f09
joaogabriel1995 <joao.pereira@gtiit.com.br>
2025-02-25 16:24:20 -0300

    Add message broker integration to HandleVoiceInputUseCase for publishing media messages

--- backend/src/application/useCases/handleVoiceInputUseCase.ts
+++ backend/src/application/useCases/handleVoiceInputUseCase.ts
@@ -1,15 +1,24 @@
 import { IMediaMessageService } from "../../domain/services/MediaMessageService";
+import { IMessageBroker } from "../../infrastructure/messaging/IMessageBroker";
+import { RabbitMQBrokerAdvanced } from "../../infrastructure/messaging/RabbitMQBroker";
 import { WebhookBodySchemaType } from "../schemas/evoluctionWebhookSchema";
 
 
+
+
 export class HandleVoiceInputUseCase {
-    constructor(private mediaMessageService: IMediaMessageService) { }
+    constructor(
+      private mediaMessageService: IMediaMessageService,
+      private messageBroker: IMessageBroker
+
+    ) { }
 
     async execute(webhookPayloadBody: WebhookBodySchemaType): Promise<void> {
         const { instance, apikey } = webhookPayloadBody
         const { id } = webhookPayloadBody.data.key
         const media = await this.mediaMessageService.getBase64FromMediaMessage(instance, id, apikey);
         console.log(media)
+        await this.messageBroker.publish('transcription_queue_base64', media)
         // const message = new Message(media);
     }
 }
