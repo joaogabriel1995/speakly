f93315ddd1861fd6b84d0c93e3a63e42a01d29af
joaogabriel1995 <joao.pereira@gtiit.com.br>
2025-02-25 23:18:25 -0300

    Add Transcriber component for YouTube to text conversion

---
+++ frontend/src/pages/transcriber/transcriber.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material"
+import { Content } from "../../components/layout/Content"
+
+
+
+
+
+export const Transcriber: React.FC = () => {
+  return (
+    <Content title="Conversor de YouTube para Texto">
+      <Box></Box>
+    </Content>)
+}
