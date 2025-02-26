8b6677f1da418aa8195753fa50a73ddca39d8b8b
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-02-26 17:13:52 -0300

    Add ConfigService for environment configuration management

---
+++ backend/src/infrastructure/services/configService.ts
@@ -0,0 +1,39 @@
+import { EnvironmentConfig, EnvironmentSchema } from "../../application/schemas/environmentSchema";
+import { EnvLoader, IEnvLoader } from "../config/EnvLoader";
+
+
+export class ConfigService {
+  private config: EnvironmentConfig | null = null;
+
+  constructor(private envLoader: IEnvLoader<EnvironmentConfig>) { }
+
+  getConfig(): EnvironmentConfig {
+    if (!this.config) {
+      this.config = this.envLoader.load();
+    }
+    return this.config;
+  }
+}
+
+export const configService = (() => {
+  let instance: ConfigService | null = null;
+
+  const createInstance = () => {
+    const envLoader = new EnvLoader(EnvironmentSchema);
+    return new ConfigService(envLoader);
+  };
+
+
+  return {
+    getInstance: (): ConfigService => {
+      if (!instance) {
+        instance = createInstance();
+        if (instance !== null) {
+          instance.getConfig()
+        }
+
+      }
+      return instance;
+    },
+  };
+})();
