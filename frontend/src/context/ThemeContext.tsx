b6043f67b5705868a5ea06bc77c71f3efb94ff9b
joaogabriel1995 <joao.pereira@gtiit.com.br>
2025-02-25 23:18:20 -0300

    Add AppThemeProvider and ThemeContext for managing light and dark themes

---
+++ frontend/src/context/ThemeContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, ReactNode, useCallback, useContext, useMemo, useState } from 'react';
+import { Box, ThemeProvider } from '@mui/material';
+import { DarkTheme } from '../assets/styles/DarkTheme';
+
+
+interface IThemeContextData {
+  themeName: 'light' | 'dark';
+  toggleTheme: () => void;
+
+}
+export interface IThemeContext {
+    children: ReactNode;
+  }
+
+const ThemeContext = createContext({} as IThemeContextData);
+
+export const useAppThemeContext = () => {
+  return useContext(ThemeContext);
+};
+
+export const AppThemeProvider: React.FC<IThemeContext> = ({ children }) => {
+  const [themeName, setThemeName] = useState<'light' | 'dark'>('light');
+
+  const toggleTheme = useCallback(() => {
+    setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light');
+  }, []);
+
+  const theme = useMemo(() => {
+    if (themeName === 'dark') return DarkTheme;
+
+    return DarkTheme;
+  }, [themeName]);
+
+
+  return (
+    <ThemeContext.Provider value={{ themeName, toggleTheme }}>
+      <ThemeProvider theme={theme}>
+        <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
+          {children}
+        </Box>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
