faf7187d51a5edc88b5a5f26ccebc198617510c1
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-02-26 17:13:58 -0300

    Refactor RabbitMQBroker initialization to use singleton pattern in Router

--- backend/src/infrastructure/server/Router.ts
+++ backend/src/infrastructure/server/Router.ts
@@ -3,22 +3,22 @@ import { WhatsAppAdapter } from "../adapters/EvolutionAppAdapter"
 import { EvolutionController } from "../../application/controllers/EvolutionController"
 import { AppConversationOrchestratorUseCase } from "../../application/useCases/chat/AppConversationOrchestratorUseCase"
 import { EvolutionMediaMessageService } from "../services/EvolutionMediaMessageService"
-import { RabbitMQBrokerAdvanced } from "../messaging/RabbitMQBroker"
 import { HandleVoiceInputUseCase } from "../../application/useCases/voice/HandleVoiceInputUseCase"
 import { HandleChatInputUseCase } from "../../application/useCases/chat/HandleChatInputUseCase"
 import { TranscriberController } from "../../application/controllers/TranscriberController"
 import { TranscriberUseCase } from "../../application/useCases/transcriber/TranscriberUseCase"
 import { TranscriptionRepoPrisma } from "../repository/transcriberPrisma"
 import { PrismaClient } from "@prisma/client"
+import { RabbitMQBrokerAdvanced } from "../messaging/RabbitMQBroker"
 
 const router = Router()
 
 router.post('/', async (request, response) => {
     const handleChatInputUseCase = new HandleChatInputUseCase()
     const evolutionMediaMessageService = new EvolutionMediaMessageService()
-    const rabbitMQBrokerAdvanced = new RabbitMQBrokerAdvanced("amqp://localhost:5672")
-    await rabbitMQBrokerAdvanced.init()
-    const handleVoiceInputUseCase = new HandleVoiceInputUseCase(evolutionMediaMessageService, rabbitMQBrokerAdvanced)
+    const rabbitMQBroker = RabbitMQBrokerAdvanced.getInstance("amqp://localhost:5672")
+    await rabbitMQBroker.init()
+    const handleVoiceInputUseCase = new HandleVoiceInputUseCase(evolutionMediaMessageService, rabbitMQBroker)
     const whatsAppAdapter = new WhatsAppAdapter()
 
     const appConversationOrchestratorUseCase = new AppConversationOrchestratorUseCase(
@@ -41,11 +41,11 @@ router.post('/', async (request, response) => {
 
 router.post('/transcription', async (request, response) => {
   console.log("ASDASDASDASDASD")
-  const rabbitMQBrokerAdvanced = new RabbitMQBrokerAdvanced("amqp://localhost:5672")
-  await rabbitMQBrokerAdvanced.init()
+  const rabbitMQBroker = RabbitMQBrokerAdvanced.getInstance("amqp://localhost:5672")
+  await rabbitMQBroker.init()
   const prisma = new PrismaClient()
   const transcriptionRepo= new TranscriptionRepoPrisma(prisma)
-  const transcriber = new TranscriberUseCase(rabbitMQBrokerAdvanced, transcriptionRepo)
+  const transcriber = new TranscriberUseCase(rabbitMQBroker, transcriptionRepo)
   const transcriberController = new TranscriberController(transcriber)
   transcriberController.handle({
     body: request.body,
