9d281759179cfde62d0a6396056015121d095f31
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-02-26 01:22:58 -0300

    Add TranscriptionSchema for validating transcription webhook data

---
+++ backend/src/application/schemas/TranscriptionSchema.ts
@@ -0,0 +1,9 @@
+import { optional, z } from "zod";
+
+// Schema principal para EvolutionWebhookBodyDTO
+export const TransctiptionSchema = z.object({
+    url: z.string(),
+});
+
+export type TransctiptionUseCaseDto = z.infer<typeof TransctiptionSchema>;
+
