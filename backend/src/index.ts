79e4a541cef2f4cc08921f80ea5a06ab68f61966
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-02-26 21:54:14 -0300

    Add consumer startup to application entry point for message processing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 // index.ts (raiz)
 
+import { startConsumer } from "./infrastructure/messaging";
 import { startServer } from "./infrastructure/server";
 
 // Ponto de entrada da aplicação
 (async () => {
   try {
     await startServer();
+    await startConsumer()
     console.log('Aplicação inicializada com sucesso');
   } catch (error) {
     console.error('Erro ao inicializar a aplicação:', error);
