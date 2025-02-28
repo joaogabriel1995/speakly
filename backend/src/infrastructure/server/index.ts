981158a67d59541cfaa2ecb08c1ee3b374f6b537
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-02-28 02:00:40 -0300

    Add CORS middleware to server for improved cross-origin requests handling

--- backend/src/infrastructure/server/index.ts
+++ backend/src/infrastructure/server/index.ts
@@ -2,6 +2,7 @@
 import express from 'express';
 import { configService } from '../services/configService'; // Ajuste o caminho
 import { router } from './Router';
+import cors from "cors";
 
 const app = express();
 
@@ -9,13 +10,18 @@ app.use(express.json());
 
 export async function startServer(): Promise<void> {
   try {
-    // Carrega e valida as configurações no início
     const config = configService.getInstance().getConfig();
     console.log('Configurações carregadas com sucesso:', config);
 
     const port = config.SERVER_PORT;
 
+    app.use(cors({
+      origin: '*', // Defina um domínio específico se necessário
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    }));
     app.use(router);
+
 
     app.listen(port, () => {
       console.log(`Server rodando na porta ${port}`);
