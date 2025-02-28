f6e41d4c191873640b086b64f9666dd5337c39bf
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-02-28 02:00:27 -0300

    Update TranscriberUseCase to publish detailed transcription data to the message broker

--- backend/src/application/useCases/transcriber/TranscriberUseCase.ts
+++ backend/src/application/useCases/transcriber/TranscriberUseCase.ts
@@ -18,7 +18,13 @@ export class TranscriberUseCase {
       trasncriberType: 'youtube',
       userId: "ee062683-856d-452e-85e5-3cd0390f7d21"
     })
-    await this.messageBroker.publish('download_queue', { url, transcriptionId: transcription.getId() })
+    await this.messageBroker.publish('ddd', {
+      url: transcription.getUrl(),
+      language: transcription.getLanguage(),
+      status: transcription.getStatus(),
+      userId: transcription.getUserId(),
+      trasncriberType: transcription.getTranscriberType()
+    })
     await this.transcriptionRepository.save(transcription)
   }
 }
