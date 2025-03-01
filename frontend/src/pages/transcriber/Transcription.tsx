d9f03bb617d4a88d89136c39053ad5bf3ff9f9ad
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-02-28 23:28:32 -0300

    Refactor Transcriber component layout for improved structure and positioning

--- frontend/src/pages/transcriber/Transcription.tsx
+++ frontend/src/pages/transcriber/Transcription.tsx
@@ -1,16 +1,27 @@
-import { Box } from "@mui/material"
-import { Content } from "../../components/layout/Content"
-import TranscriptionForm from "./TranscriptionForm"
-
-
-
-
+import { Box } from "@mui/material";
+import { Content } from "../../components/layout/Content";
+import TranscriptionForm from "./TranscriptionForm";
 
 export const Transcriber: React.FC = () => {
+
+
   return (
     <Content title="Conversor de YouTube para Texto">
-      <Box>
-        <TranscriptionForm />
+      <Box display="flex" flexDirection="row" width="100%" position="relative">
+        {/* Formulário à esquerda/esquerda-central */}
+        <Box flex="1">
+          <TranscriptionForm />
+        </Box>
+
+        <Box
+          position="fixed"
+          right={16}
+          top={80} // Ajuste conforme o header ou layout
+          zIndex={1300} // Acima de outros elementos
+          width={300} // Largura fixa do alerta
+        >
+        </Box>
       </Box>
-    </Content>)
-}
+    </Content>
+  );
+};
