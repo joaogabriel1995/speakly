3f4127903bb92e5e53d3e38c7dab5a233ed1d8c2
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-02-28 23:28:37 -0300

    Add routing for transcription and home pages in AppRoutes component

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import { Transcriber } from "../pages/transcriber/Transcription"
+import { Content } from "../components/layout/Content"
 
 
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Transcriber></Transcriber>}></Route>
+      <Route path="/transcription" element={<Transcriber></Transcriber>}></Route>
+      <Route path="/home" element={<Content title="Página Inicial"> ASIDJAOISDJOIASJD </Content>}></Route>
+
     </Routes>
   )
 }
