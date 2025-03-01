2461c6088b9a964bd0ca58effabe6674ca1cbd4f
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-02-28 23:28:06 -0300

    Enhance Content component with responsive drawer support and integrate AlertComponent

--- frontend/src/components/layout/Content.tsx
+++ frontend/src/components/layout/Content.tsx
@@ -1,4 +1,6 @@
 import { Box, Typography, useTheme } from "@mui/material"
+import { useDrawerContext } from "../../context/DrawerContext"
+import { AlertComponent } from "./alert/Alert"
 
 
 interface IContentProps {
@@ -6,19 +8,32 @@ interface IContentProps {
   children: React.ReactNode
 }
 
-
 export const Content: React.FC<IContentProps> = ({ children, title }: IContentProps) => {
-  const theme = useTheme()
+  const { isDrawerOpen } = useDrawerContext();
+  const theme = useTheme();
+  const MARGIN_LEFT = 16;
+  const drawerWidth = isDrawerOpen ? 250 + MARGIN_LEFT : 70 + MARGIN_LEFT;
+
 
   return (
-  <Box height="100%" display="flex" flexDirection="column" gap={1} bgcolor={theme.palette.background.default}>
-    <Box padding={theme.spacing(2)} height={theme.spacing(8)} textAlign={"center"} display={"flex"} alignItems={"center"}>
-      <Typography variant="h3" sx={{ marginLeft: '2.5%' }}>
-        {title}
-      </Typography>
-    </Box >
-    <Box padding={theme.spacing(2)}   flex={1} overflow="auto">
-      {children}
-    </Box>
-  </Box>)
+    <Box
+      sx={{
+        marginLeft: `${drawerWidth}px`,
+        padding: 3,
+        transition: theme.transitions.create("margin-left", {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      }}
+    >
+      <AlertComponent />
+      <Box padding={theme.spacing(2)} height={theme.spacing(8)} textAlign={"center"} display={"flex"} alignItems={"center"}>
+        <Typography variant="h3" sx={{ marginLeft: '2.5%' }}>
+          {title}
+        </Typography>
+      </Box >
+      <Box padding={theme.spacing(2)} flex={1} overflow="auto">
+        {children}
+      </Box>
+    </Box>)
 }
