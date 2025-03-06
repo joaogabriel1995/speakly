6ad5cd84cb0671abe4f418c3063c61d803273891
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-06 02:14:05 -0300

    Add CreateManyLearningJourney use case for batch creation of learning journeys

---
+++ backend/src/application/useCases/plan-study/CreateLearningJourney.ts
@@ -0,0 +1,26 @@
+import { LearningJourneyEntity } from "../../../domain/entities/LearningJourney";
+import { ILearningJourneysRepository } from "../../../domain/repository/ILearningJourneyRepository";
+import { LearningJourneyInputDto } from "../../schemas/LearningJorneyInputSchema";
+
+export class CreateManyLearningJourney {
+  constructor(private repository: ILearningJourneysRepository) { }
+
+  async execute(input: LearningJourneyInputDto[]): Promise<LearningJourneyEntity[]> {
+
+    const learningData = input.map((data) => {
+      return new LearningJourneyEntity(
+        {
+          objective: data.objective,
+          activity: data.activity,
+          week: data.week,
+          month: data.month,
+          theory: data.theory,
+        }
+      );
+    })
+
+    await this.repository.createMany(learningData);
+    return learningData;
+  }
+}
+
