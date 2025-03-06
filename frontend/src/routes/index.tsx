ab47b7ca6f3d75a408dd42a1b2736d899aa2f055
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-06 02:15:36 -0300

    Add route for PlanStudyPage in the application

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import { Transcriber } from "../pages/transcriber/Transcription"
 import { Content } from "../components/layout/Content"
+import { PlanStudyPage } from "../pages/plan-study/PlanStudy"
 
 
 
@@ -8,6 +9,8 @@ export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/transcription" element={<Transcriber></Transcriber>}></Route>
+      <Route path="/study" element={<PlanStudyPage></PlanStudyPage>}></Route>
+
       <Route path="/home" element={<Content title="Página Inicial"> ASIDJAOISDJOIASJD </Content>}></Route>
 
     </Routes>
