8fa52ea65e14a16c675a33656fc238a5ecc856b5
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-07 02:35:17 -0300

    Refactor LearningJourneyInputSchema to rename and restructure input schemas for clarity

--- backend/src/application/schemas/LearningJorneyInputSchema.ts
+++ backend/src/application/schemas/LearningJorneyInputSchema.ts
@@ -1,11 +1,22 @@
 import { z } from "zod";
 
-export const LearningJourneyInput = z.array(z.object({
+export const LearningPlanInput = z.array(z.object({
   objective: z.string(),
   activity: z.string(),
   week: z.number().int(),
   month: z.number().int(),
   theory: z.string(),
 }))
+
+
+export const LearningJourneyInput = z.object({
+  plan: LearningPlanInput,
+  userId: z.string(),
+  learningSettingsId: z.string(),
+})
+
+
+
+export type LearningPlanInputDto = z.infer<typeof LearningPlanInput>
 
 export type LearningJourneyInputDto = z.infer<typeof LearningJourneyInput>
