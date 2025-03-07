5947e7242cf276dc510576bc73ddc852219f158a
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-07 02:35:47 -0300

    Refactor PlanStudyUseCase to use LearningSettingsInputDto and update property names for consistency

--- backend/src/application/useCases/plan-study/StudyPlan.ts
+++ backend/src/application/useCases/plan-study/StudyPlan.ts
@@ -1,6 +1,6 @@
 import { Transcription } from "../../../domain/entities/Transcription";
 import { IMessageBroker } from "../../../infrastructure/messaging/IMessageBroker";
-import { PlanStudyUseCaseDto } from "../../schemas/PlanStudySchema";
+import { LearningSettingsInputDto } from "../../schemas/LearningSettingsInputSchema";
 
 
 export class PlanStudyUseCase {
@@ -8,13 +8,13 @@ export class PlanStudyUseCase {
     private readonly messageBroker: IMessageBroker,
   ) { }
 
-  async execute(data: PlanStudyUseCaseDto): Promise<void> {
+  async execute(data: LearningSettingsInputDto): Promise<void> {
 
     await this.messageBroker.publish("agent-plan-study", {
       level: data.level,
       duration: data.duration,
-      days_week: data.days_week,
-      hour_day: data.hour_day,
+      days_week: data.daysWeek,
+      hour_day: data.hourDay,
       userId: "ee062683-856d-452e-85e5-3cd0390f7d21"
     }
     )
