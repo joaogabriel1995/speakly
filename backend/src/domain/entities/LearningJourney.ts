1ec383eb46949ed85d48bebb6fdb0dd644e3986b
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-07 02:35:50 -0300

    Add userId and learningJourneyId to LearningJourneyEntity and enhance validation

--- backend/src/domain/entities/LearningJourney.ts
+++ backend/src/domain/entities/LearningJourney.ts
@@ -7,6 +7,19 @@ interface LearningJourneyEntityProps {
   week: number;
   month: number;
   theory: string;
+  userId: string;
+  learningJourneyId: string
+}
+
+enum LearningJourneyErrors {
+  OBJECTIVE_REQUIRED = "Objective is required",
+  ACTIVITY_REQUIRED = "Activity is required",
+  WEEK_REQUIRED = "Week is required",
+  MONTH_REQUIRED = "Month is required",
+  THEORY_REQUIRED = "Theory is required",
+  USER_ID_REQUIRED = "User ID is required",
+  LEARNING_SETTINGS_ID_REQUIRED = "Learning Settings ID is required"
+
 }
 
 export class LearningJourneyEntity {
@@ -16,30 +29,45 @@ export class LearningJourneyEntity {
   private week: number;
   private month: number;
   private theory: string;
+  private userId: string
+  private learningJourneyId: string
+
 
   constructor(
-    { objective, activity, week, month, theory }: Omit<LearningJourneyEntityProps, "id">,
+    { objective, activity, week, month, theory, userId, learningJourneyId }: Omit<LearningJourneyEntityProps, "id">,
     id?: string
   ) {
-    this.validateProps({ objective, activity, week, month, theory });
+    this.validateProps({ objective, activity, week, month, theory, userId, learningJourneyId });
     this.id = id ?? uuid();
     this.objective = objective;
     this.activity = activity;
     this.week = week;
     this.month = month;
     this.theory = theory;
+    this.userId = userId
+    this.learningJourneyId = learningJourneyId
   }
 
+
+
   private validateProps(props: Omit<LearningJourneyEntityProps, "id">): void {
-    if (!props.objective) throw new Error("Objective é obrigatório");
-    if (!props.activity) throw new Error("Activity é obrigatória");
-    if (!props.week) throw new Error("Week é obrigatória");
-    if (!props.month) throw new Error("Month é obrigatório");
-    if (!props.theory) throw new Error("Theory é obrigatória");
+    if (!props.objective) throw new Error(LearningJourneyErrors.OBJECTIVE_REQUIRED);
+    if (!props.activity) throw new Error(LearningJourneyErrors.ACTIVITY_REQUIRED);
+    if (!props.week) throw new Error(LearningJourneyErrors.WEEK_REQUIRED);
+    if (!props.month) throw new Error(LearningJourneyErrors.MONTH_REQUIRED);
+    if (!props.theory) throw new Error(LearningJourneyErrors.THEORY_REQUIRED);
+    if (!props.userId) throw new Error(LearningJourneyErrors.USER_ID_REQUIRED);
+    if (!props.learningJourneyId) throw new Error(LearningJourneyErrors.LEARNING_SETTINGS_ID_REQUIRED);
+
   }
+
 
   getId(): string {
     return this.id;
+  }
+
+  getUserId(): string {
+    return this.userId;
   }
 
   getObjective(): string {
@@ -62,6 +90,11 @@ export class LearningJourneyEntity {
     return this.theory;
   }
 
+  getLearningJourneyId(): string {
+    return this.learningJourneyId;
+  }
+
+
   setObjective(objective: string): void {
     if (!objective) throw new Error("Objective não pode ser vazio");
     this.objective = objective;
@@ -75,6 +108,7 @@ export class LearningJourneyEntity {
       week: this.week,
       month: this.month,
       theory: this.theory,
+      userId: this.userId
     };
   }
 }
