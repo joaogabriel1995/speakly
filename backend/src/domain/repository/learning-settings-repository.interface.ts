6aa66de7b058ac40b0244de45e48603a592b8e97
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-08 03:23:31 -0300

    Add findManyByUserId method to ILearningSettingsRepository for user-specific retrieval

--- backend/src/domain/repository/learning-settings-repository.interface.ts
+++ backend/src/domain/repository/learning-settings-repository.interface.ts
@@ -2,4 +2,6 @@ import { LearningSettingsEntity } from "../entities/learning-settings.entity";
 
 export interface ILearningSettingsRepository {
   create(learningSettings: LearningSettingsEntity): Promise<LearningSettingsEntity>;
+  findManyByUserId(userId: string): Promise<LearningSettingsEntity[]>;
+
 }
