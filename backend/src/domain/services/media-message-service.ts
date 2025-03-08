fa9def20991a4a38e6c10373fd20720f77c31b73
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-08 03:23:35 -0300

    Fix import path for MediaMessageResponseDTO in media-message-service

--- backend/src/domain/services/media-message-service.ts
+++ backend/src/domain/services/media-message-service.ts
@@ -1,4 +1,4 @@
-import { MediaMessageResponseDTO } from "../../application/schemas/media-message.schemas";
+import { MediaMessageResponseDTO } from "../../application/schemas/media-message.schema";
 
 export interface IMediaMessageService {
   getBase64FromMediaMessage(instance: string, messageID: string, apikey: string): Promise<MediaMessageResponseDTO>;
