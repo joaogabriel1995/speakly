83d6f9fd728a30bfff9acabb66698f22172204d7
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-09 04:19:45 -0300

    Update routes to import MyStudyPlans from the correct path and replace placeholder with JourneyDetails component

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,8 +2,9 @@ import { Route, Routes } from "react-router-dom"
 import { Transcriber } from "../pages/transcriber/Transcription"
 import { Content } from "../components/layout/Content"
 import { PlanStudyPage } from "../pages/plan-study/PlanStudy"
-import { MyStudyPlans } from "../pages/MyStudyPlans/MyStudyPlans"
+import { MyStudyPlans } from "../pages/myStudyPlans/MyStudyPlans"
 import { pathRoutes } from "./paths"
+import { JourneyDetails } from "../pages/journeyDetails/JourneyDetails"
 
 
 
@@ -13,7 +14,7 @@ export const AppRoutes = () => {
       <Route path={pathRoutes.TRANSCRIPTION} element={<Transcriber></Transcriber>}></Route>
       <Route path={pathRoutes.STUDY_PLAN_GENERATE} element={<PlanStudyPage></PlanStudyPage>}></Route>
       <Route path={pathRoutes.STUDY_PLAN_LIST} element={<MyStudyPlans />}></Route>
-      <Route path={pathRoutes.STUDY_PLAN_DETAILS} element={<p>ASDASDASDASDASDASDASDASDSADASD </p>}></Route>
+      <Route path={pathRoutes.STUDY_PLAN_DETAILS} element={<JourneyDetails/>}></Route>
       <Route path={pathRoutes.HOME} element={<Content title="Página Inicial"> ASIDJAOISDJOIASJD </Content>}></Route>
     </Routes>
   )
