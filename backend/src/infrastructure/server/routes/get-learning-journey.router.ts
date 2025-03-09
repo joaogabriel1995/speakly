07a325aa4e8854f8d0264822f29390fb654f920a
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-09 03:42:15 -0300

    Add router for retrieving learning journey by ID

---
+++ backend/src/infrastructure/server/routes/get-learning-journey.router.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { container } from "../container";
+
+const router = Router();
+
+router.get("/:id", async (request, response) => {
+  console.log(request)
+  const result = await container.getLearningJourneyByIdController.handle({
+    body: request.body,
+    method: request.method,
+    headers: request.headers,
+    params: request.params
+  });
+  response.status(result.statusCode).json(result.body);
+});
+
+export { router as getLearningJourneyByIdRouter };
