c40cffa849a79d286aab6b4518022f68fe9c60fb
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-09 01:17:59 -0300

    Add pathRoutes enum for centralized route management

---
+++ frontend/src/routes/paths.ts
@@ -0,0 +1,8 @@
+
+export enum pathRoutes {
+  TRANSCRIPTION = '/transcription',
+  STUDY_PLAN_GENERATE = '/study-plan/generate',
+  STUDY_PLAN_LIST = '/study-plan/list',
+  STUDY_PLAN_DETAILS = '/study-plan/:planId',
+  HOME = '/home'
+}
