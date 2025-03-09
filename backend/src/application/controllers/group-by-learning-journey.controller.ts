b9143ebbb5a53f1230b30db49de9a0360995477d
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-09 03:41:25 -0300

    Add GetLearningJourneyByIdController for handling learning journey retrieval

---
+++ backend/src/application/controllers/group-by-learning-journey.controller.ts
@@ -0,0 +1,35 @@
+// src/infrastructure/controllers/get-learning-journey-by-id.controller.ts
+import {
+  IController,
+  IRequest,
+  IResponse,
+} from "../interfaces/adapters/controller.interface";
+import { GetLearningJourneyByIdInput } from "../schemas/group-by-learning-journey-by-id-input";
+import { GroupBySettingLearningJourneyByIdUseCase } from "../useCases/learningJourney/get-learning-journey.use-case";
+
+export class GetLearningJourneyByIdController implements IController {
+  constructor(
+    private readonly getLearningJourneyByIdUseCase: GroupBySettingLearningJourneyByIdUseCase,
+  ) { }
+
+  async handle(request: IRequest<unknown>): Promise<IResponse<unknown>> {
+    try {
+      // Os parâmetros chegam em `request.params`
+      console.log(request.params)
+      const input = GetLearningJourneyByIdInput.parse(request.params );
+
+      const learningJourney = await this.getLearningJourneyByIdUseCase.execute(input);
+
+      return {
+        body: learningJourney,
+        statusCode: 200,
+      };
+    } catch (error: unknown) {
+      console.log(error)
+      return {
+        body: (error as Error).message,
+        statusCode: 400,
+      };
+    }
+  }
+}
