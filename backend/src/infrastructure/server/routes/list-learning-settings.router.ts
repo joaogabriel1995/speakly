26785e4d8712aa4f9b692ee53f481d66f8f46510
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-09 01:17:29 -0300

    Add listLearningSettings router to handle POST requests

---
+++ backend/src/infrastructure/server/routes/list-learning-settings.router.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { container } from "../container";
+
+const router = Router();
+
+router.post("/", async (request, response) => {
+  const result = await container.listLearningSettingsController.handle({
+    body: request.body,
+    method: request.method,
+    headers: request.headers,
+  });
+  response.status(result.statusCode).json(result.body);
+});
+
+export { router as listLearningSettingsRouter };
