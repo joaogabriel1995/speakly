fdfa0d2c9bd125fa5e765f2bf849f6088434b53b
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-09 01:17:55 -0300

    Refactor routing paths to use constants from pathRoutes for better maintainability

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,18 +2,19 @@ import { Route, Routes } from "react-router-dom"
 import { Transcriber } from "../pages/transcriber/Transcription"
 import { Content } from "../components/layout/Content"
 import { PlanStudyPage } from "../pages/plan-study/PlanStudy"
-import { MyStudyPlans } from "../pages/plan-study/MyStudyPlans"
+import { MyStudyPlans } from "../pages/MyStudyPlans/MyStudyPlans"
+import { pathRoutes } from "./paths"
 
 
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/transcription" element={<Transcriber></Transcriber>}></Route>
-      <Route path="/study-plan/generate" element={<PlanStudyPage></PlanStudyPage>}></Route>
-      <Route path="/study-plan/list" element={<MyStudyPlans />}></Route>
-      <Route path="/study-plan/:planId" element={<p>ASDASDASDASDASDASDASDASDSADASD </p>}></Route>
-      <Route path="/home" element={<Content title="Página Inicial"> ASIDJAOISDJOIASJD </Content>}></Route>
+      <Route path={pathRoutes.TRANSCRIPTION} element={<Transcriber></Transcriber>}></Route>
+      <Route path={pathRoutes.STUDY_PLAN_DETAILS} element={<PlanStudyPage></PlanStudyPage>}></Route>
+      <Route path={pathRoutes.STUDY_PLAN_GENERATE} element={<MyStudyPlans />}></Route>
+      <Route path={pathRoutes.STUDY_PLAN_LIST} element={<p>ASDASDASDASDASDASDASDASDSADASD </p>}></Route>
+      <Route path={pathRoutes.HOME}element={<Content title="Página Inicial"> ASIDJAOISDJOIASJD </Content>}></Route>
 
     </Routes>
   )
