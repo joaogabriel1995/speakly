f13295fb4f60bf00cebcdfe1e3ac8283d4bbbd00
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-19 00:41:54 -0300

    Add route for detailed learning plan creation

--- backend/src/infrastructure/server/routes/router.ts
+++ backend/src/infrastructure/server/routes/router.ts
@@ -5,6 +5,7 @@ import { transcriptionRouter } from "./transcription.routes";
 import { studyRouter } from "./study.routes";
 import { listLearningSettingsRouter } from "./list-learning-settings.router";
 import { getLearningJourneyByIdRouter } from "./get-learning-journey.router";
+import { createDetailedLearningPlanRouter } from "./create-learning-detailed-learning.router";
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.use("/transcription", transcriptionRouter);
 router.use("/study", studyRouter);
 router.use("/learning-settings", listLearningSettingsRouter);
 router.use("/learning-journey", getLearningJourneyByIdRouter);
+router.use("/learning-detail", createDetailedLearningPlanRouter);
 
 export { router };
