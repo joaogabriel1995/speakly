91bcfddc5238d322c7b3ae9d223de8bb4c7abb79
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-21 01:00:10 -0300

    Add routes for retrieving tasks by journey ID and listening lesson by ID

--- backend/src/infrastructure/server/routes/router.ts
+++ backend/src/infrastructure/server/routes/router.ts
@@ -6,6 +6,8 @@ import { studyRouter } from "./study.routes";
 import { listLearningSettingsRouter } from "./list-learning-settings.router";
 import { getLearningJourneyByIdRouter } from "./get-learning-journey.router";
 import { createDetailedLearningPlanRouter } from "./create-learning-detailed-learning.router";
+import { findManyTasksByJourneyRouter } from "./find-many-tasks-by-journey.router";
+import { getListeningLeasonByIdRouter } from "./get-listening-leason-by-id.router";
 
 const router = Router();
 
@@ -16,5 +18,9 @@ router.use("/study", studyRouter);
 router.use("/learning-settings", listLearningSettingsRouter);
 router.use("/learning-journey", getLearningJourneyByIdRouter);
 router.use("/learning-detail", createDetailedLearningPlanRouter);
+router.use("/tasks", findManyTasksByJourneyRouter);
+router.use("/listening-leason", getListeningLeasonByIdRouter);
+
+
 
 export { router };
