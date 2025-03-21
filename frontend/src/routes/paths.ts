ca7a4eff53d4c08211f27fae3e141a352c5cdb3a
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-21 01:00:30 -0300

    Add TASKS route for accessing tasks based on learning settings and detail ID

--- frontend/src/routes/paths.ts
+++ frontend/src/routes/paths.ts
@@ -4,5 +4,6 @@ export enum pathRoutes {
   STUDY_PLAN_GENERATE = '/study-plan/generate',
   STUDY_PLAN_LIST = '/study-plan/list',
   STUDY_PLAN_DETAILS = '/learning_journey/:learningSettingsId',
-  HOME = '/home'
+  HOME = '/home',
+  TASKS = 'learning_journey/:learningSettingsId/tasks/:learningDetailId',
 }
