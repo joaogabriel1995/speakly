e385057beed7330cd995217fc813e3bc021481da
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-21 00:59:23 -0300

    Add findManyByJourney method to ITaskRepository for retrieving tasks by journey ID

--- backend/src/domain/repository/task-repository.interface.ts
+++ backend/src/domain/repository/task-repository.interface.ts
@@ -2,4 +2,5 @@ import { TaskEntity } from "../entities/task.entity";
 
 export interface ITaskRepository {
   createMany(message: TaskEntity[]): Promise<TaskEntity[]>;
+  findManyByJourney(journeyId: string): Promise<TaskEntity[]>;
 }
