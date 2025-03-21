051be76f94781d1e3e1b3f4e130c54eecd5971f5
joaogabriel1995 <joaogabrielrp@gmail.com>
2025-03-21 00:59:54 -0300

    Refactor TaskRepoPrisma to use createManyAndReturn for task creation and enhance findManyByJourney method for improved task retrieval by journey ID

--- backend/src/infrastructure/repository/task.prisma.ts
+++ backend/src/infrastructure/repository/task.prisma.ts
@@ -1,17 +1,10 @@
 import { Prisma, PrismaClient } from "@prisma/client";
-import { TaskEntity } from "../../domain/entities/task.entity";
+import { SkillEnum, TaskEntity, TaskStatusEnum } from "../../domain/entities/task.entity";
 import { ITaskRepository } from "../../domain/repository/task-repository.interface";
-import { connect } from "http2";
-
-
-
-
-
-
 
 
 export class TaskRepoPrisma implements ITaskRepository {
-  constructor(private prisma: PrismaClient) {}
+  constructor(private prisma: PrismaClient) { }
 
   async createMany(tasks: TaskEntity[]): Promise<TaskEntity[]> {
     const data = tasks.map((value) => {
@@ -23,13 +16,40 @@ export class TaskRepoPrisma implements ITaskRepository {
         repetitions: value.getRepetitions(),
         status: value.getStatus(),
         learningJourneyId: value.getLearningJourneyId(),
-        content: String(value.getContent()),
+        // content: String(value.getContent()),
         day: value.getDay(),
       };
       return data;
     });
-    await this.prisma.task.createMany({ data });
-    return tasks;
+    const tasksPrisma = await this.prisma.task.createManyAndReturn({ data });
+    return tasksPrisma.map((value) => new TaskEntity({
+      day: value.day,
+      duration: value.duration,
+      learningJourneyId: value.learningJourneyId,
+      repetitions: value.repetitions,
+      resource: value.resource,
+      skill: value.skill as unknown as SkillEnum,
+      status: value.status as unknown as TaskStatusEnum,
+      task: value.task,
+    }, value.id));
+
+  }
+  async findManyByJourney(journeyId: string): Promise<TaskEntity[]> {
+    const tasks = await this.prisma.task.findMany({
+      where: {
+        learningJourney: { id: { equals: journeyId } }
+      }
+    })
+    return tasks.map((value) => new TaskEntity({
+      day: value.day,
+      duration: value.duration,
+      learningJourneyId: value.learningJourneyId,
+      repetitions: value.repetitions,
+      resource: value.resource,
+      skill: value.skill as unknown as SkillEnum,
+      status: value.status as unknown as TaskStatusEnum,
+      task: value.task,
+    }, value.id));
   }
 
 }
